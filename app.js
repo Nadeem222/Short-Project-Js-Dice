f4c6f613249be0c1a992d3ec6af3a24a2c8f1276
Nadeem Hakeem <110339895+Nadeem222@users.noreply.github.com>
2023-12-27 22:39:49 +0500

    some work

--- app.js
+++ app.js
@@ -19,33 +19,33 @@ function anim() {
     image.setAttribute("src", "dice.gif");
 }
 
-// document.getElementById('generate').addEventListener('click', function () {
-//     var length = document.getElementById('length').value;
-//     var uppercase = document.getElementById('uppercase').checked;
-//     var lowercase = document.getElementById('lowercase').checked;
-//     var numbers = document.getElementById('numbers').checked;
-//     var symbols = document.getElementById('symbols').checked;
+document.getElementById('generate').addEventListener('click', function () {
+    var length = document.getElementById('length').value;
+    var uppercase = document.getElementById('uppercase').checked;
+    var lowercase = document.getElementById('lowercase').checked;
+    var numbers = document.getElementById('numbers').checked;
+    var symbols = document.getElementById('symbols').checked;
 
-//     document.getElementById('password').value = generatePassword(length, uppercase, lowercase, numbers, symbols);
-// });
-// function generatePassword(length, uppercase, lowercase, numbers, symbols) {
-//     var charset = '';
-//     var password = '';
+    document.getElementById('password').value = generatePassword(length, uppercase, lowercase, numbers, symbols);
+});
+function generatePassword(length, uppercase, lowercase, numbers, symbols) {
+    var charset = '';
+    var password = '';
 
-//     if (uppercase) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-//     if (lowercase) charset += 'abcdefghijklmnopqrstuvwxyz';
-//     if (numbers) charset += '0123456789';
-//     if (symbols) charset += '!@#$%^&*()_+[]{}|;:,.<>?';
+    if (uppercase) charset += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (lowercase) charset += 'abcdefghijklmnopqrstuvwxyz';
+    if (numbers) charset += '0123456789';
+    if (symbols) charset += '!@#$%^&*()_+[]{}|;:,.<>?';
 
-//     for (var i = 0; i < length; i++) {
-//         var randomIndex = Math.floor(Math.random() * charset.length);
-//         password += charset.charAt(randomIndex);
+    for (var i = 0; i < length; i++) {
+        var randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset.charAt(randomIndex);
 
-//         // console.log(randomIndex);
-//     }
+        // console.log(randomIndex);
+    }
 
-//     return password;
-// }
+    return password;
+}
 
 
 
